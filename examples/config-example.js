8c700547aa88caa83b95daaaf21e03aa1f2323f1
Night Raven <zkjcksn@gmail.com>
2021-11-30 07:17:28 -0500

    Initial Commit

---
+++ examples/config-example.js
@@ -0,0 +1,7 @@
+exports.TOKEN = "";
+exports.PREFIX = '.';
+exports.MOBILE = false;
+exports.STREAMING = false;
+exports.NORMAL = true;
+exports.shards = 1; // MUST be atleast 1.
+exports.OWNER_ID = ""; // required (dont ask why but its needed as well in owner.json)
